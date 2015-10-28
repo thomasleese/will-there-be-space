edc7e6f75928c7bbfbdb80f619745cf56b19f97b
Thomas Leese <inbox@thomasleese.me>
2015-10-28 09:49:11 +0000

    Update migrations

--- migrations/versions/3cdfbb1dd1e_create_place_tables.py
+++ migrations/versions/3cdfbb1dd1e_create_place_tables.py
@@ -18,14 +18,46 @@ depends_on = None
 
 def upgrade():
     op.create_table(
+        'author',
+        sa.Column('id', sa.Integer, primary_key=True, nullable=False),
+        sa.Column('account_id', sa.Integer, nullable=True),
+        sa.Column('ip_address', sa.String, nullable=False),
+        sa.Column('date', sa.DateTime, nullable=False),
+    )
+
+    op.create_table(
+        'approval',
+        sa.Column('id', sa.Integer, primary_key=True, nullable=False),
+        sa.Column('account_id', sa.Integer, sa.ForeignKey('account.id'),
+                  nullable=True),
+        sa.Column('date', sa.DateTime, nullable=True),
+    )
+
+    op.create_table(
         'place',
         sa.Column('id', sa.Integer, primary_key=True, nullable=False),
         sa.Column('slug', sa.String, unique=True, nullable=False),
         sa.Column('name', sa.String, nullable=False),
         sa.Column('description', sa.String, nullable=False),
-        sa.Column('creation_date', sa.DateTime, nullable=False),
-        sa.Column('creator_id', sa.Integer, sa.ForeignKey('account.id'),
+        sa.Column('location', sa.String, nullable=False),
+        sa.Column('author_id', sa.Integer, sa.ForeignKey('author.id'),
                   nullable=False),
+        sa.Column('approval_id', sa.Integer, sa.ForeignKey('approval.id'),
+                  nullable=True),
+    )
+
+    op.create_table(
+        'place_change',
+        sa.Column('id', sa.Integer, primary_key=True, nullable=False),
+        sa.Column('place_id', sa.Integer, sa.ForeignKey('place.id'),
+                  nullable=False),
+        sa.Column('author_id', sa.Integer, sa.ForeignKey('author.id'),
+                  nullable=False),
+        sa.Column('column', sa.String, nullable=False),
+        sa.Column('old_value', sa.String, nullable=False),
+        sa.Column('new_value', sa.String, nullable=False),
+        sa.Column('approval_id', sa.Integer, sa.ForeignKey('approval.id'),
+                  nullable=True),
     )
 
     op.create_table(
@@ -33,14 +65,17 @@ def upgrade():
         sa.Column('id', sa.Integer, primary_key=True, nullable=False),
         sa.Column('place_id', sa.Integer, sa.ForeignKey('place.id'),
                   nullable=False),
-        sa.Column('account_id', sa.Integer, sa.ForeignKey('account.id'),
+        sa.Column('author_id', sa.Integer, sa.ForeignKey('author.id'),
                   nullable=False),
         sa.Column('date', sa.DateTime, unique=True, nullable=False),
         sa.Column('used_spaces', sa.Integer, nullable=False),
         sa.Column('free_spaces', sa.Integer, nullable=False),
+        sa.Column('approval_id', sa.Integer, sa.ForeignKey('approval.id'),
+                  nullable=True),
     )
 
 
 def downgrade():
     op.drop_table('place_update')
     op.drop_table('place')
+    op.drop_table('author')
