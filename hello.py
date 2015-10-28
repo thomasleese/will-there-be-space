848fe4948849018499a2361fd4faa3e86830b5d4
Thomas Leese <inbox@thomasleese.me>
2015-10-28 09:02:47 +0000

    Add place route

--- hello.py
+++ hello.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Will there be a space?'
+
+
+@app.route('/in/<place_name>')
+def place(place_name):
+    return 'I doubt it.'
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
