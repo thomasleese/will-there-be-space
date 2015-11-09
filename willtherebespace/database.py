6aa5201c4432dfd3f93162c52cdb352eb7f03b51
Thomas Leese <inbox@thomasleese.me>
2015-11-09 16:19:02 +0000

    Add connection pooling

--- willtherebespace/database.py
+++ willtherebespace/database.py
@@ -65,6 +65,22 @@ def get_sql_connection():
     return _sql_connection
 
 
+@sqlalchemy.event.listens_for(sqlalchemy.pool.Pool, 'checkout')
+def sql_ping_connection(dbapi_connection, connection_record, connection_proxy):
+    cursor = dbapi_connection.cursor()
+    try:
+        cursor.execute("SELECT 1")
+    except:
+        # optional - dispose the whole pool
+        # instead of invalidating one at a time
+        # connection_proxy._pool.dispose()
+
+        # raise DisconnectionError - pool will try
+        # connecting again up to three times before raising.
+        raise sqlalchemy.exc.DisconnectionError()
+    cursor.close()
+
+
 def get_redis_uri(key=REDIS_URI_KEY):
     try:
         return os.environ[key]
